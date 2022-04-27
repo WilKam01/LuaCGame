0f39b5d88df77970d96fe43073038fd196074c93
William Kamnert <wille.kamnert@gmail.com>
2022-04-27 20:23:56 +0200

    EnTT support
    Added support for EnTT. Behaviour from the ECS later will be exposed to lua.

--- LuaCGame/Application/main.cpp
+++ LuaCGame/Application/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include "lua.hpp"
 #include "raylib.h"
+#include "entt.hpp"
 
 int main()
 {
+    srand((unsigned int)time(0));
+
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dostring(L, "print('Hello Lua!')");
+
+    entt::registry registry;
+    for (int i = 0; i < 100; i++)
+    {
+        entt::entity entity = registry.create();
+        registry.emplace<int>(entity, rand() % 25);
+    }
+
+    auto view = registry.view<int>();
+    view.each([](int num) {
+        std::cout << num << std::endl;
+    });
 
 	InitWindow(800, 450, "raylib basic window");
 	SetTargetFPS(60);
