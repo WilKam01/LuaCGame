efc824254ab5591bac3686ed60c6191e69d67157
William Kamnert <wille.kamnert@gmail.com>
2022-04-27 17:36:08 +0200

    Raylib support
    Added raylib support. Changed it so both Lua and Raylib are separate projects within the same solution. Both compile static libraries used in the Application project. Works for x64 and x86, debug and release.

--- LuaCGame/Application/main.cpp
+++ LuaCGame/Application/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include "lua.hpp"
+#include "raylib.h"
 
 int main()
 {
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dostring(L, "print('Hello Lua!')");
+
+	InitWindow(800, 450, "raylib basic window");
+	SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
 	return 0;
 }
